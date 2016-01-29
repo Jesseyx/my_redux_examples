b4ff627745fedf9de6669f6e8e0d5c7f5270758a
Jesseyx <jyaoxiao@hotmail.com>
2016-01-29 21:09:01 +0800

    ...

---
+++ todo/server.js
@@ -0,0 +1,24 @@
+var webpack = require('webpack');
+var webpackDevMiddleware = require('webpack-dev-middleware');
+var webpackHotMiddleware = require('webpack-hot-middleware');
+var config = require('./webpack.config');
+
+var app = new (require('express'))();
+var port = 3000;
+
+var compiler = webpack(config);
+
+app.use(webpackDevMiddleware(compiler, { noInfo: true,  publicPath: config.output.publicPath }));
+app.use(webpackHotMiddleware(compiler));
+
+app.get('/', function (req, res) {
+  console.log('index.');
+});
+
+app.listen(port, function (error) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.info("==>     Listening on port %s. Open up http://localhost:%s/ in your browser.", port, port);
+  }
+});
