a89b2ed2cac97c7c6ecc7add524edaa6666f0bdc
Jesseyx <jyaoxiao@hotmail.com>
2016-02-17 18:17:42 +0800

    Done!

--- reddit_api/index.js
+++ reddit_api/index.js
@@ -1,22 +1,10 @@
 import 'babel-polyfill';
 
-import thunkMiddleware from 'redux-thunk';
-import createLogger from 'redux-logger';
-import { createStore, applyMiddleware } from 'redux';
-import { selectSubreddit, fetchPosts } from './actions';
-import rootReducer from './reducers';
+import React from 'react';
+import { render } from 'react-dom';
+import Root from './containers/Root';
 
-const loggerMiddleware = createLogger();
-
-const createStoreWithMiddleware = applyMiddleware(
-  thunkMiddleware,            // 允许我们 dispatch() 的函数
-  loggerMiddleware            // 一个很便捷的 middleware，用来在客户端打印 action 日志
-)(createStore);
-
-const store = createStoreWithMiddleware(rootReducer);
-
-store.dispatch(selectSubreddit('reactjs'));
-store.dispatch(selectSubreddit('reactjs'));
-store.dispatch(fetchPosts('reactjs')).then(() => 
-  console.log(store.getState())
+render(
+  <Root />,
+  document.getElementById('root')
 );
