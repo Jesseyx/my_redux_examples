4c6d3407b799e147cd3419459d0be939340e3b7d
Jesseyx <jyaoxiao@hotmail.com>
2016-03-02 18:13:25 +0800

    setp-6 real-world

--- real-world/middleware/api.js
+++ real-world/middleware/api.js
@@ -1,11 +1,29 @@
 import { Schema, arrayOf, normalize } from 'normalizr';
+// 将 key 序列化成驼峰格式
+import { camelizeKeys } from 'humps';
+import fetch from 'isomorphic-fetch';
 
 const API_ROOT = 'https://api.github.com/';
 
 // Fetches an API response and normalizes the result JSON according to schema.
 // This makes every API response have the same shape, regardless of how nested it was.
 function callApi(endpoint, schema) {
+  const fullUrl = (endpoint.indexOf(API_ROOT) === -1) ? API_ROOT + endpoint : endpoint;
 
+  return fetch(fullUrl)
+    .then(response => response.json().then(json => ({ json, response })))
+    .then(({ json, response }) => {
+      if (!response.ok) {
+        return Promise.reject(json);
+      }
+
+      const camelizedJson = camelizeKeys(json);
+      console.log(camelizedJson);
+      // const nextPageUrl = getNextPageUrl(response)
+
+      console.log(normalize(camelizedJson, schema));
+      return Object.assign({}, normalize(camelizedJson, schema), { });
+    });
 }
 
 const userSchema = new Schema('users', {
@@ -49,7 +67,7 @@ export default store => next => action => {
     throw new Error('Expected an array of three action types.');
   }
 
-  if (!types.eveny(type => typeof type === 'string')) {
+  if (!types.every(type => typeof type === 'string')) {
     throw new Error('Expected action types to be strings.');
   }
 
@@ -62,5 +80,14 @@ export default store => next => action => {
   const [ requestType, successType, failureType ] = types;
   next(actionWith({ type: requestType }));
 
-  return callApi(endpoint, schema).then();
+  return callApi(endpoint, schema).then(
+    response => next(actionWith({
+      response,
+      type: successType
+    })),
+    error => next(actionWith({
+      type: failureType,
+      error: error.message || 'Something bad happened'
+    }))
+  );
 }
