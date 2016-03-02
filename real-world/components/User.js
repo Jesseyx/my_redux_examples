fc2f8e59819fff4a05319e5328abd6e68fbebdc4
Jesseyx <jyaoxiao@hotmail.com>
2016-03-02 21:15:28 +0800

    setp-7 real-world

--- real-world/components/User.js
+++ real-world/components/User.js
@@ -3,13 +3,13 @@ import { Link } from 'react-router';
 
 export default class User extends Component {
   render() {
-    const { login } = this.props.user;
+    const { login, avatarUrl, name } = this.props.user;
     return (
       <div className="User">
         <Link to={ `/${ login }` }>
-          <img src="https://avatars2.githubusercontent.com/u/13999774?v=3&s=72" width="72" height="72" />
+          <img src={ avatarUrl } width="72" height="72" />
           <h3>
-            { login } 
+            { login } { name && <span>({ name })</span>}
           </h3>
         </Link>
       </div>
@@ -18,5 +18,9 @@ export default class User extends Component {
 }
 
 User.propTypes = {
-  user: PropTypes.object.isRequired
+  user: PropTypes.shape({
+    login: PropTypes.string.isRequired,
+    avatarUrl: PropTypes.string.isRequired,
+    name: PropTypes.string
+  }).isRequired
 };
