5664951d4001e47f69d517b259eb4c7046896e5d
Jesseyx <jyaoxiao@hotmail.com>
2016-03-21 21:29:12 +0800

    Add test part

--- todomvc/test/components/MainSection.spec.js
+++ todomvc/test/components/MainSection.spec.js
@@ -43,7 +43,36 @@ describe('components', () => {
   describe('MainSection', () => {
     it('should render container', () => {
       const { output } = setup();
+      expect(output.type).toBe('section');
+      expect(output.props.className).toBe('main');
+    })
 
+    describe('toggle all input', () => {
+      it('should render', () => {
+        const { output } = setup();
+        const [ toggle ] = output.props.children;
+        expect(toggle.type).toBe('input');
+        expect(toggle.props.type).toBe('checkbox');
+        expect(toggle.props.checked).toBe(false);
+      })
+
+      it('should be checked if all todos completed', () => {
+        const { output } = setup({
+          todos: [
+            { text: 'Use Redux', completed: true, id: 0 }
+          ]
+        });
+
+        const [ toggle ] = output.props.children;
+        expect(toggle.props.checked).toBe(true);
+      })
+
+      it('should call completeAll on change', () => {
+        const { output, props } = setup();
+        const [ toggle ] = output.props.children;
+        toggle.props.onChange({});
+        expect(props.actions.completeAll).toHaveBeenCalled();
+      })
     })
   })
 })
