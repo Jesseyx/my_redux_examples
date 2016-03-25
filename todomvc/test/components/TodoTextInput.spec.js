0c59b12bb259b9d78a17c3a9026e2366a5acd745
Jesseyx <jyaoxiao@hotmail.com>
2016-03-25 19:50:44 +0800

    Done TodoTextInputs's test

--- todomvc/test/components/TodoTextInput.spec.js
+++ todomvc/test/components/TodoTextInput.spec.js
@@ -37,9 +37,45 @@ describe('components', () => {
     })
 
     it('should render correctly when editing=true', () => {
+      const { output } = setup({ editing: true });
+      expect(output.props.className).toEqual('edit');
+    })
 
+    it('should render correctly when newTodo=true', () => {
+      const { output } = setup({ newTodo: true });
+      expect(output.props.className).toEqual('new-todo');
+    })
+
+    it('should update value on change', () => {
+      const { output, renderer } = setup();
+      output.props.onChange({ target: { value: 'Use Radox' }});
+      const updated = renderer.getRenderOutput();
+      expect(updated.props.value).toEqual('Use Radox');
+    })
+
+    it('should call onSave on enter key press', () => {
+      const { output, props } = setup();
+      output.props.onKeyDown({ target: { value: 'Use Redux' }, which: 13 });
+      expect(props.onSave).toHaveBeenCalledWith('Use Redux');
+    })
+
+    it('should reset state on enter key press if newTodo', () => {
+      const { output, renderer } = setup({ newTodo: true });
+      output.props.onKeyDown({ target: { value: 'Use Redux' }, which: 13 });
+      const updated = renderer.getRenderOutput();
+      expect(updated.props.value).toEqual('');
+    })
+
+    it('should call onSave on blue', () => {
+      const { output, props } = setup();
+      output.props.onBlur({ target: { value: 'Use Redux' }});
+      expect(props.onSave).toHaveBeenCalledWith('Use Redux');
+    })
+
+    it('shouldnt call onSave on blur if newTodo', () => {
+      const { output, props } = setup({ newTodo: true });
+      output.props.onBlur({ target: { value: 'Use Redux' }});
+      expect(props.onSave.calls.length).toBe(0);
     })
   })
 })
-
-NODE_ENV=production pm2 start
